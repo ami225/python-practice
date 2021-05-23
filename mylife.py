d6cc414d0c0a310c45387285fce73cd74f700c4d
ami225 <ami2kawabe25@gmail.com>
2021-05-23 07:06:45 +0000

    [Add]曜日ごとにメッセージ

--- mylife.py
+++ mylife.py
@@ -13,4 +13,20 @@ print(str(life.days) + "日間生きています！")
 
 #現在の年齢
 age = math.floor(life.days / 365)
-print(str(age) + "歳で")
+print(str(age) + "歳です")
+
+#曜日ごとにメッセージを変える
+#0は月曜日、6は日曜日
+
+# 月曜日
+if today.weekday() == 0:
+    print("一週間の幕開け！！")
+# 火曜 ~ 木曜日
+elif today.weekday() < 4:
+    print("がんばれ！！！")
+# 金曜日
+elif today.weekday() == 4:
+    print("金曜日だ！！あとちょっと！")
+# 土日
+else:
+    print("死ぬほど楽しめ！！！")
